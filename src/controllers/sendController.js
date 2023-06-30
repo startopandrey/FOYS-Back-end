32ff053700241adb7c7e1e645dc02b932412a8e6
Andrey Ilyukhin <startopandrey@gmail.com>
2023-06-30 18:56:37 +0300

    calculation

--- src/controllers/sendController.js
+++ src/controllers/sendController.js
@@ -42,6 +42,49 @@ const sendEmail = async (req, res) => {
   }
   return res.status(200).json({ message: message });
 };
+const sendCalculation = async (req, res) => {
+  const message = req.body;
+  console.log(message);
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    port: 465,
+    host: "smtp.gmail.com",
+    auth: {
+      user: "foyb.development@gmail.com",
+      pass: "sdvldalcrbukevyn",
+      //  process.env.NODE_ENV == "development"
+      //   ? "rlrortswrgnjsvap"
+      //   : "sdvldalcrbukevyn",
+      //   dwstcvocneuzifts for heroku
+    },
+  });
+
+  if (message) {
+    try {
+      const emailOptions = {
+        from: "andrey01test@gmail.com",
+        to: "focusonyoursoftware@gmail.com",
+        subject: "NEW PROJECT HAVE BEEN SUBMITED, WAKE UP, BRO ;)",
+        html: `<p>${message}</p>`,
+      };
+
+      const data = await transporter.sendMail(emailOptions);
+      if (data) {
+        return res.status(200).json({
+          status: "SUCCESS",
+          message: "Email Has Been Sent",
+          data,
+        });
+      } else {
+        return res.status(400);
+      }
+    } catch (error) {
+      return res.status(400);
+    }
+  }
+  return res.status(200).json({ message: message });
+};
 const sendHire = async (req, res) => {
   const message = req.body.message;
   console.log(message);
@@ -85,4 +128,4 @@ const sendHire = async (req, res) => {
   }
   return res.status(200).json({ message: message });
 };
-module.exports = { sendEmail, sendHire };
+module.exports = { sendEmail, sendHire, sendCalculation };
--- src/routes/send.js
+++ src/routes/send.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const sendController = require("../controllers/sendController");
 
 router.post("/email", sendController.sendEmail);
+router.post("/calculation", sendController.sendCalculation);
 
 router.post("/hire", sendController.sendHire);
 
